555c2992850ffcfd5eeffd81d7cb1250ea42ffdf
nvzt13 <nevzatatalay79@gmail.com>
2024-10-16 17:30:20 +0300

    update app.js

--- 1-) Palindrome/app.js
+++ 1-) Palindrome/app.js
@@ -6,24 +6,31 @@ button.addEventListener('click', getValue);
 
 function getValue() {
   if (input.value.trim().length == 0) {
-    result.innerText = 'Please input a value '
+    alert('Please input a value')
   }
   else {
-    
-  let text = input.value;
- // This will output "string"
+  let text = input.value.toLowerCase();
   checkInputValue(text)
   }
 }
 
 function checkInputValue(str) {
   // Reverse the string
-  const reversedStr = str.split('').reverse().join('');
-
+  
+  const reversedStr = str.split('').reverse().join('').toLowerCase();
+  const cleanText = reversedStr.replace(/\s/g, '');
+console.log(str.split(' ').join(''))
   // Check if it's a palindrome
-  if (str === reversedStr) {
+  if (str == reversedStr){
      result.innerText = `${str} is a palindrome`;
-  } else {
+  }else if(reversedStr.match(/_/gi)) {
+     result.innerText = `${str} is a palindrome`;
+  } else if (str.split(' ').join('') == cleanText) {
+result.innerText = `${str} is a palindrome`;
+  }else if(reversedStr.match(/.,/gi)){
+       result.innerText = `${str} is a palindrome`;
+  }
+  else {
     result.innerText = (`${str} is not a Palindrome`);
   }
 }
